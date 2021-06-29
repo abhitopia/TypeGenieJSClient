52f313979f18ff07e34793bf6ef820fde13bc138
Renato Rocha <renato@typegenie.net>
2021-06-29 22:04:28 +0100

    Implemented changes according to feedback from meeting (28/06/2021). Added keystroke to telemetry, implemented selection of highlighted text, html garbage is removed before text analysis. Some bugs are still persisitng and highlighted text is still not working for multiple lines

--- src/telemetry/telemetry_buffer.ts
+++ src/telemetry/telemetry_buffer.ts
@@ -6,7 +6,8 @@ interface StateIteration  {
     anchorPosition : {[axis: string]: number};
     timestamp : number,
     textDiff: any,
-    availableCompletion : string
+    availableCompletion : string,
+    keyStroke: {key: string, altKey: boolean, ctrlKey: boolean, shiftKey: boolean}
 }
 
 interface SessionHistory {
@@ -39,27 +40,7 @@ const reconstructTextFromElementCollection = (text_wrapper: HTMLCollectionOf<Ele
 
 
 // Processes text differences and computes caret position
-const processTextEditing = (prev: string, current: string) => {
-        prev = prev ? prev : "";
-        current = current ? current : "";
-        // console.log(`Computing difference between ${prev} and ${current}`);
-        const computeDiff = diffChars(prev, current);
-        const result : any = {textDiff: null, yOffset: null, xOffset: null};
-        if(computeDiff.length >= 2) {
-            const textBeforeEditPosition = computeDiff[0]?.value?.split('\n');
-            const currentLine = textBeforeEditPosition?.length -1;
-            let caretOffset;
-            if(computeDiff[1].removed) {
-                caretOffset = textBeforeEditPosition[currentLine].length + computeDiff[1].count;
-            } else {
-                caretOffset = textBeforeEditPosition[currentLine].length;
-            }
 
-            Object.assign(result,{textDiff: computeDiff[1],
-                yOffset: currentLine, xOffset: caretOffset})
-        }
-        return result;
-}
 
 // Bootstraps completion html from remaining text
 const bootstrapCompletion = (htmlDoc: any, completionClass: string) : string => {
@@ -90,9 +71,9 @@ export class TypeGenieTelemetryBuffer implements TypeGenieTelemetryBufferInterfa
     public sessionHistory : SessionHistory;
     private readonly editorScope: Element;
     private currentHtmlInnerState: string;
-    private currentText: string;
+    private currentText: string = "";
     private readonly completionClass: string;
-    private previousCaretPosition : any = {x: null, y: null};
+    private currentSelection : any;
 
     set sessionId(value: string) {
         this.sessionHistory.session_id = value
@@ -103,25 +84,18 @@ export class TypeGenieTelemetryBuffer implements TypeGenieTelemetryBufferInterfa
     }
 
 
+
+
     constructor(private editor: any, editorScope: Element) {
         this.editor = editor;
         this.sessionHistory = {session_id: null, stateTransitionHistory: []};
         this.editorScope = editorScope;
         this.completionClass = "tg-completion";
-        let mutationObserver = new MutationObserver(() => this.updateEditorStateTransitionHistory());
-        mutationObserver.observe(this.editorScope, {
-            attributes: true,
-            childList: true,
-            characterData: true,
-            subtree: true,
-            characterDataOldValue: true,
-            attributeOldValue: true
-        });
+        this.editorScope.addEventListener('keyup', (e) => this.updateEditorStateTransitionHistory(e as KeyboardEvent))
+        this.editorScope.addEventListener('mouseup', ()=> this.setCurrentSelection())
+        this.editorScope.addEventListener('keydown', ()=> this.setCurrentSelection())
         this.initTelemetryReport(5000);
     }
-
-
-
 
 
     resetStateHistory() {
@@ -129,22 +103,57 @@ export class TypeGenieTelemetryBuffer implements TypeGenieTelemetryBufferInterfa
     }
 
 
-    updateEditorStateTransitionHistory() {
+    updateEditorStateTransitionHistory(event: KeyboardEvent) {
+        console.log('On update state transition history selection is: ', this.currentSelection);
         if((this.currentHtmlInnerState !== this.editorScope.innerHTML)) {
             const {text, completionText} = digestEditorScopeHtml(this.editorScope.innerHTML, this.completionClass);
-            let {textDiff, yOffset, xOffset} = processTextEditing(this.currentText, text);
+            let {textDiff, caret} = this.processTextEditing(text, event.key);
             this.sessionHistory.stateTransitionHistory.push({
-                anchorPosition: {x: xOffset, y: yOffset}, timestamp: Date.now(), textDiff: textDiff,
+                anchorPosition: caret, timestamp: Date.now(), textDiff: textDiff,
                 availableCompletion: completionText,
+                keyStroke: {key:event.key, shiftKey:event.shiftKey, altKey:event.altKey, ctrlKey: event.ctrlKey}
             })
-            // if(xOffset && yOffset) {
-            //     this.previousCaretPosition = {x: xOffset, y: yOffset};
-            // }
             this.currentHtmlInnerState = this.editorScope.innerHTML;
             this.currentText = text;
         }
     }
 
+
+    processTextEditing = (current: string, keyStroke: string) => {
+        current = current ? current : "";
+        const computedDiff = diffChars(this.currentText , current);
+        const result : any = {textDiff: null, caret: {anchorOffset: null, focusOffset: null}};
+        if(computedDiff.length >= 2) {
+            const currentText = computedDiff[0].value;
+            const removedByBackspace = computedDiff[1].removed && keyStroke === 'Backspace';
+            const caretPosition = removedByBackspace? currentText.length + computedDiff[1].count : currentText.length;
+            // const highlightedSelection = this.computeMultilineCaretOffset(computedDiff);
+            // Try to work around this special case
+            // Object.assign(result,{textDiff: computedDiff[1],
+            //     caret: highlightedSelection ? highlightedSelection: {anchorOffset: caretPosition}})
+            Object.assign(result,{textDiff: computedDiff[1],
+                caret: this.currentSelection ? this.currentSelection: {anchorOffset: caretPosition}})
+        }
+        return result;
+    }
+
+
+    setCurrentSelection() {
+        if(document.getSelection().anchorNode != document.getSelection().focusNode) {
+            console.log('Computing selection for different nodes');
+            console.log('Anchor node: ', document.getSelection().anchorNode);
+            console.log('Anchor offset: ', document.getSelection().anchorOffset);
+            console.log('Focus node: ', document.getSelection().focusNode);
+            console.log('Focus offset: ', document.getSelection().focusOffset);
+
+        } else {
+            if(document.getSelection().anchorOffset != document.getSelection().focusOffset) {
+                this.currentSelection = {anchorOffset: document.getSelection().anchorOffset, focusOffset: document.getSelection().focusOffset}
+            } else {
+                this.currentSelection = null;
+            }
+        }
+    }
 
     initTelemetryReport(interval: number) {
         const context = this;
